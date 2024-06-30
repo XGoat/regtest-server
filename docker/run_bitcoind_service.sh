3afa2eab1f90e37f6c599938c3579e9fca963fb9
Ubuntu <ubuntu@ip-172-31-0-75.us-east-2.compute.internal>
2024-06-30 09:48:17 +0000

    run node

--- docker/run_bitcoind_service.sh
+++ docker/run_bitcoind_service.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-/usr/bin/bitcoind -server -regtest -txindex -zmqpubhashtx=tcp://127.0.0.1:30001 -zmqpubhashblock=tcp://127.0.0.1:30001 -rpcworkqueue=32 &
+/usr/bin/bitcoind -server -regtest -txindex -zmqpubhashtx=tcp://127.0.0.1:30001 -zmqpubhashblock=tcp://127.0.0.1:30001 -rpcworkqueue=32 -rpcbind='0.0.0.0' -rpcallowip='0.0.0.0/0'  -fallbackfee='0.01' -rpcuser=111111 -rpcpassword=111111 &
+
 disown
 sleep 2
 /usr/bin/bitcoin-cli -regtest createwallet default
--- rpc.js
+++ rpc.js
@@ -18,7 +18,7 @@ if (!auth) {
 }
 
 module.exports = require('yajrpc/qup')({
-  url: process.env.RPC || 'http://localhost:8332',
+  url: process.env.RPC || 'http://localhost:18443',
   auth: auth,
   batch: process.env.RPCBATCHSIZE || 500,
   concurrent: process.env.RPCCONCURRENT || 16
